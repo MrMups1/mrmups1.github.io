49eddbe38dadc486e813cc321ec016c66c9872b8
MrMups1 <95277067+MrMups1@users.noreply.github.com>
2021-12-13 15:20:34 +0000

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ answers = answers.sort(() => Math.random() - 0.5)
 function showQuestion() {
     
     currentQuestion = answers[Math.floor(Math.random() * answers.length)];//selects a random array entry
-    console.log (answers[currentQuestion])
+    console.log.answers[currentQuestion]
     if((answers[currentQuestion]) === 'undefined') { //if the selector hits a missing element, redo
         currentQuestion ++;
         showQuestion ();}
